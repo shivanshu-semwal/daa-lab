f94e449f3840a5f9d14bbbab8c097c4cb2d4e68c
shivanshu-semwal <semwalshivanshu@gmail.com>
2021-08-11 12:56:58 +0530

    add week2 sol

--- week1/binary_search.cpp
+++ week1/binary_search.cpp
@@ -12,40 +12,40 @@
 using namespace std;
 
 int main() {
-  int t;
-  cin >> t;
-  while (t--) {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-      cin >> v[i];
-    }
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
 
-    int key;
-    cin >> key;
+        int key;
+        cin >> key;
 
-    int start = 0, end = n - 1;
-    int mid = (start + end) / 2;
+        int start = 0, end = n - 1;
+        int mid = (start + end) / 2;
 
-    bool flag = false;
-    int count = 0;
-    while (start <= end) {
-      mid = (start + end) / 2;
-      if (++count && v[mid] == key) {
-        flag = true;
-        break;
-      } else if (v[mid] < key) {
-        start = mid + 1;
-      } else {
-        end = mid - 1;
-      }
+        bool flag = false;
+        int count = 0;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (++count && v[mid] == key) {
+                flag = true;
+                break;
+            } else if (v[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        if (flag) {
+            cout << "Present " << count << "\n";
+        } else {
+            cout << "Not Present " << count << "\n";
+        }
     }
-    if (flag) {
-      cout << "Present " << count << "\n";
-    } else {
-      cout << "Not Present " << count << "\n";
-    }
-  }
-  return 0;
+    return 0;
 }
--- week1/linear_search.cpp
+++ week1/linear_search.cpp
@@ -12,30 +12,30 @@
 using namespace std;
 
 int main() {
-  int t;
-  cin >> t;
-  while (t--) {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-      cin >> v[i];
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        int key;
+        cin >> key;
+        int count = 0;
+        bool flag = false;
+        for (int i = 0; i < v.size(); i++) {
+            if (++count && v[i] == key) {
+                flag = true;
+                break;
+            }
+        }
+        if (flag) {
+            cout << "Present " << count << endl;
+        } else {
+            cout << "Not Present " << count << endl;
+        }
     }
-    int key;
-    cin >> key;
-    int count = 0;
-    bool flag = false;
-    for (int i = 0; i < v.size(); i++) {
-      if (++count && v[i] == key) {
-        flag = true;
-        break;
-      }
-    }
-    if (flag) {
-      cout << "Present " << count << endl;
-    } else {
-      cout << "Not Present " << count << endl;
-    }
-  }
-  return 0;
+    return 0;
 }
